bf6db681cd84d194cfd36ee53459f9bd7785b3a8
namoopsoo <namoopsoo@users.noreply.github.com>
2020-10-22 13:11:25 -0400

    Create argparse_boilerplate.py

---
+++ python/argparse_boilerplate.py
@@ -0,0 +1,33 @@
+import argparse
+
+def bake_options():
+    return [
+            [['--verbose', '-v'],
+                {'action': 'store_true',
+                    'help': 'pass to to be verbose with commands'},
+                ],
+            [['--dry-run', '-D'],
+                {'action': 'store_true',
+                    'help': 'Dry run. Just print the command.  '},]
+                ]
+    ##
+    #             help='',
+    #             default='',
+    #             required='',
+    #             choices='',
+    #             action='',
+    #             type='',
+            
+
+def do():
+    parser = argparse.ArgumentParser()
+
+    [parser.add_argument(*x[0], **x[1])
+            for x in bake_options()]
+
+    # Collect args from user.
+    args = parser.parse_args()
+
+    print vars(args)
+    
+do()
